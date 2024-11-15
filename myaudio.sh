616b88e4d95c7d87209a386df9d085b7c16c6aab
ben <442226+blite@users.noreply.github.com>
2024-11-15 12:57:13 -0600

    myaudio build

---
+++ myaudio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./macos.sh \
+    --enable-macos-audiotoolbox \
+    --enable-macos-avfoundation \
+    --enable-macos-bzip2 \
+    --enable-macos-zlib \
+    --enable-gpl \
+    --enable-lame \
+    --enable-opus \
+    --enable-soxr \
+    --xcframework \
+    --speed \
+
